70b25ac80c74004360c7867c04871e940d0f1682
GC Neal Clarin <gcnealchristian.clarin@g.msuiit.edu.ph>
2024-02-26 23:51:44 +0800

    wtforms tutorial

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -6,6 +9,13 @@ load_dotenv()
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "This is a Secret Key"
+
+# Create a Form class
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 
 # Create a route decorator
@@ -48,3 +58,13 @@ def page_not_found(e):
 def page_not_found(e):
     return render_template('500.html'), 500
 
+# Create name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NameForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('name.html', name=name, form=form)
